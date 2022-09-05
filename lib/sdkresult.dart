7bcea56381183873fe5d18ecb7e113f3f7e0bdae
Deniz Can <deniz@amani.tech>
2022-09-05 15:29:23 +0300

    Update rules type and set defaults for verificationCompleted and isTokenExpired

--- lib/sdkresult.dart
+++ lib/sdkresult.dart
@@ -10,16 +10,16 @@ class SdkResult {
 
   /// This List will be filled if user canceled the KYC process,
   /// or failed the verification
-  Map<String, dynamic>? rules;
+  List<dynamic>? rules;
 
   SdkResult(this.isVerificationCompleted, this.isTokenExpired,
       this.apiExceptionCode, this.rules);
 
   factory SdkResult.fromJson(dynamic json) {
     return SdkResult(
-        json['isVerificationCompleted'] as bool,
-        json['isTokenExpired'] as bool,
+        json['isVerificationCompleted'] ?? false,
+        json['isTokenExpired'] ?? false,
         json['apiExceptionCode'] as int?,
-        jsonDecode(jsonEncode(json['rules'])) as Map<String, dynamic>?);
+        jsonDecode(jsonEncode(json['rules'])));
   }
 }
