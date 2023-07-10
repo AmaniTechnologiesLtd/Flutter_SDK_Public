abdc6e323ef3f62b9803ad662cecdfa57ab611fb
Deniz Can <deniz@amani.tech>
2023-07-10 14:14:01 +0300

    update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -33,17 +33,10 @@ class _MyAppState extends State<MyApp> {
               child: const Text("Start Button"),
               onPressed: () async {
                 var result = await _amanisdkPlugin.startAmaniSDKWithToken(
-                    server: "https://dev.amani.ai",
-                    token:
-                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg5NTg4ODc4LCJpYXQiOjE2ODg5ODQwNzgsImp0aSI6ImYxY2Y5MzY2NzVhZDQxNGFhZmFhMDljNjQ5MDk4NGRjIiwidXNlcl9pZCI6IjA5YmZhOWM2LWZhYmMtNDYwNy1iMDM3LTc5NWQ1N2UzNGNjYyIsImNvbXBhbnlfaWQiOiI0ZTNmZGY2Mi01N2RjLTQzNTgtOTdkMi1mOGI1Njg4ZGNmYjciLCJwcm9maWxlX2lkIjoiMzZhNjc5NGMtMjczNy00YzUwLThmY2MtNzlhNjUwYmY0ZGM3IiwiYXBpX3VzZXIiOmZhbHNlfQ.AdvRBqqa9CEGEUc--KCZbfmGs-qIbtoRfaQqAho_2Ic",
-                    id: "456456456");
+                    server: "https://example.amani.ai",
+                    token: "customer profile token",
+                    id: "customer id card number");
                 print(result.isTokenExpired);
-                // var result = await _amanisdkPlugin.startAmaniSDKWithCredentials(
-                //     server: "https://dev.amani.ai",
-                //     loginEmail: "deniz@amani.ai",
-                //     loginPassword: "uecJQ\*B47\+\$QVW",
-                //     id: "454545");
-                // print(result.isTokenExpired);
               }),
         ),
       ),
